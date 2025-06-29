51b204bfa82f23de82272261d980873e0ebd1503
raelaraujo0 <raellyedson1@gmail.com>
2025-06-28 21:41:52 -0300

    criado a classe repository herdada de sqlalchemy com seus metodos

--- backend/src/adapters/repositories/agendamento_repository.py
+++ backend/src/adapters/repositories/agendamento_repository.py
@@ -44,3 +44,19 @@ class AbstractPecasDoAgendamentoRepository():
     @abstractmethod
     def consultar_por_agendamento(self, agendamento_id: str) -> list[PecaDoAgendamento]:
         raise NotImplementedError
+    
+class AgendamentoRepository(AbstractAgendamentoRepository, AbstractSQLAlchemyRepository):
+    def adicionar(self, agendamento: Agendamento):
+        self.session.add(agendamento)
+
+    def remover(self, agendamento: Agendamento):
+        self.session.delete(agendamento)
+    
+    def salvar(self, agendamento: Agendamento):
+        self.session.merge(agendamento)
+
+    def consultar(self, id:str) -> Agendamento | None:
+        return self.session.query(Agendamento).filter(Agendamento.id == id).first()
+
+    def consultar_por_dia(self, data) -> list[PecaDoAgendamento]:
+        return self.session.query(PecaDoAgendamento).filter(PecaDoAgendamento.data == data).all()
