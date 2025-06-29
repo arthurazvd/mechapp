f5d705778038891cf556072c39fd8d0998a4cbaa
raelaraujo0 <raellyedson1@gmail.com>
2025-06-28 21:43:10 -0300

    criado a classe repository para oficina herdada de sqlalchemy com seus metodos

--- backend/src/adapters/repositories/oficina_repository.py
+++ backend/src/adapters/repositories/oficina_repository.py
@@ -26,3 +26,23 @@ class AbstractOficinaRepository():
     @abstractmethod
     def consultar_por_proprietario(self, proprietario_id: str) -> list[Oficina]:
         raise NotImplementedError
+    
+class OficinaRepository(AbstractOficinaRepository, AbstractSQLAlchemyRepository):
+    def adicionar(self, oficina: Oficina):
+        self.session.add(oficina)
+    
+    def remover(self, oficina: Oficina):
+        self.session.delete(oficina)
+    
+    def salvar(self, oficina: Oficina):
+        self.session.merge(oficina)
+    
+    def consultar(self, id: str) -> Oficina | None:
+        self.session.query(Oficina).filter(Oficina.id == id).first()
+        return self.session.query(Oficina).filter(Oficina.id == id).first()
+    
+    def consultar_por_cnpj(self, cnpj: str) -> Oficina | None:
+        return self.session.query(Oficina).filter(Oficina.cnpj == cnpj).first()
+
+    def consultar_por_proprietario(self, proprietario_id: str) -> list[Oficina]:
+        return self.session.query(Oficina).filter(Oficina.proprietario_id == proprietario_id).all()
