c1a323e05a2a98cf00b4270ed28b1f0d4f77050e
raelaraujo0 <raellyedson1@gmail.com>
2025-06-28 21:44:04 -0300

    criado a classe repository para servico herdada de sqlalchemy com seus metodos

--- backend/src/adapters/repositories/servico_repository.py
+++ backend/src/adapters/repositories/servico_repository.py
@@ -22,3 +22,19 @@ class AbstractServicoRepository():
     @abstractmethod
     def consultar_por_nome(self, nome: str) -> list[Agendamento]:
         raise NotImplementedError
+    
+class ServicoRepository(AbstractServicoRepository, AbstractSQLAlchemyRepository):
+    def adicionar(self, servico: Agendamento):
+        self.session.add(servico)
+
+    def remover(self, servico: Agendamento):
+        self.session.delete(servico)
+    
+    def salvar(self, servico: Agendamento):
+        self.session.merge(servico)
+
+    def consultar(self, id: str) -> Agendamento | None:
+        return self.session.query(Agendamento).filter(Agendamento.id == id).first()
+    
+    def consultar_por_nome(self, nome: str) -> list[Agendamento]:
+        return self.session.query(Agendamento).filter(Agendamento.nome == nome).all()
