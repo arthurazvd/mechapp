03ef467f2996039bfa2d3db655cd853bccf9de98
raelaraujo0 <raellyedson1@gmail.com>
2025-06-28 21:42:48 -0300

    criado a classe repository para avaliacao herdada de sqlalchemy com seus metodos

--- backend/src/adapters/repositories/avaliacao_repository.py
+++ backend/src/adapters/repositories/avaliacao_repository.py
@@ -26,3 +26,23 @@ class AbstractAvaliacaoRepository():
     @abstractmethod
     def consultar_por_cliente(self, cliente_id: str) -> list[Avaliacao]:
         raise NotImplementedError
+    
+class AvaliacaoRepository(AbstractAvaliacaoRepository, AbstractSQLAlchemyRepository):
+    def adicionar(self, avaliacao: Avaliacao):
+        self.session.add(avaliacao)
+
+    def remover(self, avaliacao: Avaliacao):
+        self.session.delete(avaliacao)
+    
+    def salvar(self, avaliacao: Avaliacao):
+        self.session.merge(avaliacao)
+
+    def consultar(self, id: str) -> Avaliacao | None:
+        avaliacao = self.session.query(Avaliacao).filter(Avaliacao.id == id).first()
+        return avaliacao
+
+    def consultar_por_servico(self, servico_id: str) -> list[Avaliacao]:
+        return self.session.query(Avaliacao).filter(Avaliacao.servico_id == servico_id).all() 
+    
+    def consultar_por_cliente(self, cliente_id: str) -> list[Avaliacao]:
+        return self.session.query(Avaliacao).filter(Avaliacao.cliente_id == cliente_id).all()
